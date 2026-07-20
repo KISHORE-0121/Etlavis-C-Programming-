28b7010eaf95af3c2d44ab9dfcd9a5cde458106c
KISHORE-0121 <kishorerangaraju@gmail.com>
2026-07-20 20:36:57 +0530

    files added

---
+++ ASSESMENT_05/PROGRAM_13.C
@@ -0,0 +1,19 @@
+//  Write a program to get two numbers from user and print the HCF of those numbers.
+
+#include <stdio.h>
+
+int main ()
+{
+    int a,b,i,c;
+    scanf("%d %d",&a,&b);
+    for(i=1;i <=a && i<=b;i++)
+    {
+        if(a%i==0 && b%i==0)
+        {
+          c=i;
+          
+        }
+
+    }
+    printf("%d",c);
+}
